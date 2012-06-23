14f1dc66ff86f3d6bffd325e2b27f09431f57456
yuh <yuh@ubuntu-64-serversman.(none)>
2012-06-23 22:20:47 +0900

    7章まで完了

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,10 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.where(:done => false)
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Hinagiku::Application.routes.draw do
+  get "tasks/show"
+
+  root :to => "tasks#index"
   resources :tasks
 end
